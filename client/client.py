28fa518b906e401721f366c288ffc25d542a65cb
Шапринская Лиза <lizashapr@yandex.ru>
2026-01-14 19:17:24 +0300

    В клиент добавлены функции по реализации запросов NLTK

--- client/client.py
+++ client/client.py
@@ -1,31 +1,61 @@
-#TODO Написать программу на Python с методом main для запросов к серверу.
-
 import json
-from urllib.request import urlopen, Request
-
-url = "http://127.0.0.1:8001/"
-
-with urlopen(url) as response:
-    body = response.read()          # bytes
-    result = json.loads(body.decode())  # dict
-
-print(result)
+from urllib.request import Request, urlopen
 
 
-url1 = "http://127.0.0.1:8001/bagofwords"
+def send_request(url, payload):
+    data = json.dumps(payload).encode("utf-8")
+    req = Request(url, data=data, headers={"Content-Type": "application/json"})
 
-# Пример текста для обработки
-payload = {
-    "documents": ["Привет! Это пример текста для преобразования с помощью мешка слов."]
-}
+    with urlopen(req) as response:
+        body = response.read()
+        result = json.loads(body.decode("utf-8"))
 
-# Подготовка запроса
-data = json.dumps(payload).encode('utf-8')
-req = Request(url1, data=data, headers={'Content-Type': 'application/json'})
+    return result
 
-# Отправка запроса и получение ответа
-with urlopen(req) as response:
-    body = response.read()  # bytes
-    result = json.loads(body.decode('utf-8'))  # dict
+def pretty_print(data):
+    print(json.dumps(data, indent=2, ensure_ascii=False))
 
-print(result)
+def main():
+    base_url = "http://127.0.0.1:8001"
+
+    payload = {
+        "documents": [
+            "Привет! Это пример текста для обработки с помощью клиент-серверной программы.", 
+            "Я Лиза, мне шестнадцать лет, я люблю читать, аааааааа"
+             ]
+    }
+
+    # tokenize
+    url_tokenize = f"{base_url}/textnltk/tokenize"
+    print("TOKENIZE:")
+    pretty_print(send_request(url_tokenize, payload))
+    print()
+
+    # stem
+    url_stem = f"{base_url}/textnltk/stem?stemmer_type=porter"
+    print("STEM:")
+    pretty_print(send_request(url_stem, payload))
+    print()
+
+    # lemmatize
+    url_lemmatize = f"{base_url}/textnltk/lemmatize?pos_tagging=true"
+    print("LEMMATIZE:")
+    pretty_print(send_request(url_lemmatize, payload))
+    print()
+
+    # postag
+    url_postag = f"{base_url}/textnltk/postag?detailed=false"
+    print("POSTAG:")
+    pretty_print(send_request(url_postag, payload))
+    print()
+
+    # ner
+    url_ner = f"{base_url}/textnltk/ner?binary=false"
+    print("NER:")
+    pretty_print(send_request(url_ner, payload))
+    print()
+
+
+
+if __name__ == "__main__":
+    main()
